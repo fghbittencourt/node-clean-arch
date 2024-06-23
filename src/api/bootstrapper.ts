6f4762129c3849b61bf78de7c83fd4bb2938ea20
Fernando Bittencourt <aaronmorlocks@gmail.com>
2024-06-23 14:04:42 -0300

    feat: add dummy messenger handlers to bootstrap

--- src/api/bootstrapper.ts
+++ src/api/bootstrapper.ts
@@ -1,9 +1,31 @@
 import { DependencyContainer } from 'tsyringe';
 import Logger from '../infrastructure/log/logger';
+import DummyPublisher from '../infrastructure/messaging/publisher/dummyPublisher';
+import Publisher from '../infrastructure/messaging/publisher/publisher';
+import DummySender from '../infrastructure/messaging/sender/dummySender';
+import Sender from '../infrastructure/messaging/sender/sender';
+
+const registerMessageSender = async (
+  container: DependencyContainer
+): Promise<void> => {
+  container.register<Sender>('Sender', {
+    useClass: DummySender
+  });
+};
+
+const registerMessagePublisher = async (
+  container: DependencyContainer
+): Promise<void> => {
+  container.register<Publisher>('Publisher', {
+    useClass: DummyPublisher
+  });
+};
 
 export default async (container: DependencyContainer): Promise<void> => {
   Logger.debug('Bootstrapper initializing...');
   // Be careful, the order of these calls are important
+  await registerMessageSender(container);
+  await registerMessagePublisher(container);
 
   Logger.debug('Bootstrapper initialized!');
 };
