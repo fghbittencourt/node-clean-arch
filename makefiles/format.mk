e483fc892afc34d1462a4ba1a8506f9bbab10cda
Fernando Bittencourt <aaronmorlocks@gmail.com>
2024-06-25 22:43:47 -0300

    chore: bump eslint and remove prettier

--- makefiles/format.mk
+++ makefiles/format.mk
@@ -3,7 +3,7 @@ format: format.ts ## Format all files
 format.check: format.check-ts ## Check if every file are formatted
 
 format.ts: ## Format every project files
-	pnpm prettier --write .
+	pnpm eslint --fix .
 
 format.check-ts: ## Check if every ts file are formatted
-	pnpm prettier --check src tests && pnpm eslint src tests
+	pnpm eslint src tests
