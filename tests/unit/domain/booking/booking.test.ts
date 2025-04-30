46c5ae7a026828443c3e7923f59fd40820bee75c
Fernando Bittencourt <aaronmorlocks@gmail.com>
2025-04-30 14:30:37 -0300

    chore: stryker - mutations

--- tests/unit/domain/booking/booking.test.ts
+++ tests/unit/domain/booking/booking.test.ts
@@ -81,3 +81,6 @@ describe('Booking testing', () => {
     expect(booking.status).toBe(BookingStatus.CONFIRMED)
   })
 })
+
+// jest.useFakeTimers()
+// jest.setSystemTime(new Date('2025-04-30T12:00:00Z'))
